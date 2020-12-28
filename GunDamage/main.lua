1580a86482295f753151436295151dc1a73d064c
omghackerman <60672202+omghackerman@users.noreply.github.com>
2020-12-28 16:06:42 +0000

    Update main.lua

--- GunDamage/main.lua
+++ GunDamage/main.lua
@@ -1,5 +1,6 @@
 Citizen.CreateThread(function()
     local fire = 2 --Place Holder
+    local weaponDurability = 100
     while true do
         Citizen.Wait(1)
         if IsControlJustReleased(1, fire) then
