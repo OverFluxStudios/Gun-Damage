9c8f660fc17be62f91be90ba2e60324eb0245918
omghackerman <60672202+omghackerman@users.noreply.github.com>
2020-12-28 16:19:17 +0000

    Update main.lua

--- GunDamage/main.lua
+++ GunDamage/main.lua
@@ -1,11 +1,12 @@
 Citizen.CreateThread(function()
     local fireBtn = 69
-    local player = GetPlayerPed(-1)
     local weaponDurability = 100
     while true do
         Citizen.Wait(1)
         if IsControlJustReleased(1, fireBtn) then
-            notify("Ez Clap")
+            if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey("weapon_pistol") then
+                notify("Ez Clap")
+            end
         end
     end
 end)
