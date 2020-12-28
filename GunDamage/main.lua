72e78f54b17035e70a5afb5b84f0820be6f463a0
omghackerman <60672202+omghackerman@users.noreply.github.com>
2020-12-28 17:22:05 +0000

    Removes Weapon

--- GunDamage/main.lua
+++ GunDamage/main.lua
@@ -1,16 +1,22 @@
 Citizen.CreateThread(function()
     local fireBtn = 69
-    local weaponDurability = 100
     while true do
         Citizen.Wait(1)
         if IsControlJustReleased(1, fireBtn) then
             local weapondmg = math.random(20)
             if GetSelectedPedWeapon(GetPlayerPed(-1)) == GetHashKey("weapon_pistol") then
+                local canFire = true
+                local weaponDurability = 100
                 weaponDurability = weaponDurability - weapondmg
                 notify(weaponDurability)
                 if weaponDurability <= 0 then
-                    DisablePlayerFiring(GetPlayerPed(-1), true)
                     alert("~r~Weapon is too damaged!")
+                    RemoveWeaponFromPed(GetPlayerPed(-1), GetHashKey("weapon_pistol"))
+                    canFire = false
+                    while canFire == false do
+                        Citizen.Wait(1)
+                        DisablePlayerFiring(GetPlayerPed(-1), true)
+                    end
                 end
             end
         end
